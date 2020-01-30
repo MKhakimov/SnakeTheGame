6ae2e7fdbf85d8974a1ea76a76e72bb64dbf3471
MKhakimov <mail_zanyat@list.ru>
2020-01-30 22:37:09 +0500

    to be cont...

--- test.js
+++ test.js
@@ -1,9 +1,7 @@
 const elementSnake = document.getElementById("snakeToMove");
 const elementMouse = document.getElementById("mouseToMove");
-//elementMouse.style.marginTop = 0 + "px";
-//elementMouse.style.marginLeft = 0 + "px";
-//elementSnake.style.marginTop = snakeArr[0] + "px";
-//elementSnake.style.marginLeft = snakeArr[1] + "px";
+console.log(elementMouse);
+
 
 const makeObjSnake = (x, y) => {
   return {
@@ -33,6 +31,9 @@ let objMouse = {
   x: 3,
   y: 3
 }
+moveMouse();
+elementMouse.style.marginTop = (objMouse.x)*step + "px";
+elementMouse.style.marginLeft = (objMouse.y)*step + "px";
 
 
 function key(b){
@@ -86,6 +87,8 @@ function controlSnake(axis, vector){
       console.log(JSON.stringify(objMouse));
         if (objSnake[0].x === objMouse.x && objSnake[0].y === objMouse.y){
             moveMouse();
+            elementMouse.style.marginTop = (objMouse.x)*step + "px";
+            elementMouse.style.marginLeft = (objMouse.y)*step + "px";
             console.log('mouse now in ', JSON.stringify(objMouse));
             objSnake.push({x:0, y:0});
             console.log('push');
@@ -98,26 +101,23 @@ function controlSnake(axis, vector){
   }
   console.log(JSON.stringify(objSnake));
   if (objSnake[0].x !== area.WIDTH && objSnake[0].y !== (area.HEIGHT) && objSnake[0].x !== area.WIDTH*(-1) && objSnake[0].y !== (area.HEIGHT*-1)){
-    //тут может быть ваша ф-я осущ-ая прорисовку
+    elementSnake.style.marginLeft = (objSnake[0].x)*step + "px";
+    elementSnake.style.marginTop = (objSnake[0].y)*step + "px";
   }
   else {
     alert("You lose");
   }
 }
-//function consoleLog(a, b) { console.log(a, b);}
 
 const myConstolSnake = function() {
-  return controlSnake(axi, dir);////
+  return controlSnake(axi, dir);
 };
-//let time = setInterval(myConstolSnake, 1500);
-//let time = setInterval(controlSnake(axi, dir), 1500);
-//let timerId = setInterval(console.log(axi), 1500);
 
-let timerId = setInterval(myConstolSnake, 1500);
 
-//setInterval(() => console.log("woohoo"), 1500)
+let timerId = setInterval(myConstolSnake, 1000);
+
 function moveMouse() {
-  //console.log('Оп Бля');
+  console.log('Оп Бля');
     objMouse.x = getRandomInt(-8 , 8);
     elementMouse.style.marginLeft = objMouse[0] + "px";
     objMouse.y = getRandomInt(-6, 6);
