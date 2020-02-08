bb9108df796af94677d51beea4ff82320334f3a5
MKhakimov <mail_zanyat@list.ru>
2020-02-08 21:24:58 +0500

    head and mouse displaying

--- test.js
+++ test.js
@@ -1,7 +1,5 @@
 const elementSnake = document.getElementById("snakeToMove");
 const elementMouse = document.getElementById("mouseToMove");
-console.log(elementMouse);
-
 
 const makeObjSnake = (x, y) => {
   return {
@@ -15,8 +13,8 @@ const keys = {
   RIGHT: 39,
   DOWN: 40
 }
-const area = { //высотка и ширина в шагах
-  WIDTH: 6 ,
+const area = { //высота и ширина в шагах
+  WIDTH: 8 ,
   HEIGHT: 8
 }
 
@@ -30,18 +28,16 @@ let tick = 0;
 let objMouse = {
   x: 3,
   y: 3
-}
-moveMouse();
-elementMouse.style.marginTop = (objMouse.x)*step + "px";
-elementMouse.style.marginLeft = (objMouse.y)*step + "px";
+};
+//moveMouse();
+elementMouse.style.marginTop = (objMouse.y)*step + "px";
+elementMouse.style.marginLeft = (objMouse.x)*step + "px";
 
 
 function key(b){
   switch(b.keyCode){
     case keys.LEFT:
-      console.log('case1');
       if (lastMove !== 'right'){
-          console.log('case2');
         lastMove = 'left';
         axi = 'x';
         dir = -1;
@@ -69,10 +65,9 @@ function key(b){
       }
     break;
   }
-}
+};
 
 function controlSnake(axis, vector){
-  console.log('go!');
   if (tick===0){ //начало, логическое добавление головы
     objSnake.push({x:0, y:0});
     objSnake[0][axis] += vector;
@@ -81,53 +76,47 @@ function controlSnake(axis, vector){
     let prevTile;
     for (let i=0; i<objSnake.length; i += 1){
       let currTile = Object.assign({}, objSnake[i]);
-      if (i===0){ //направление и движение головы
+      if (i===0){ //направление и движение головы + поимка мыши
         objSnake[i][axis] += vector;
-      console.log(JSON.stringify(objSnake[i]));
-      console.log(JSON.stringify(objMouse));
+        console.log('SNAKE IN: ', JSON.stringify(objSnake));
+        console.log('MOUSE IN: ', JSON.stringify(objMouse));
         if (objSnake[0].x === objMouse.x && objSnake[0].y === objMouse.y){
             moveMouse();
-            elementMouse.style.marginTop = (objMouse.x)*step + "px";
-            elementMouse.style.marginLeft = (objMouse.y)*step + "px";
-            console.log('mouse now in ', JSON.stringify(objMouse));
             objSnake.push({x:0, y:0});
-            console.log('push');
+            console.log('PUSH!');
         }
       } else { //движение тела перестановкой значений
-        objSnake[i] = prevTile;
-      }
+          objSnake[i] = prevTile;
+          }
       prevTile = currTile;
-    }  console.log(JSON.stringify(objSnake));
+    }
+    if (objSnake[0].x !== area.WIDTH && objSnake[0].y !== (area.HEIGHT) && objSnake[0].x !== area.WIDTH*(-1) && objSnake[0].y !== (area.HEIGHT*-1)){
+      elementSnake.style.marginLeft = (objSnake[0].x)*step + "px";
+      elementSnake.style.marginTop = (objSnake[0].y)*step + "px";
+    }
+    else {
+      alert("You lose");
+    }
   }
-  console.log(JSON.stringify(objSnake));
-  if (objSnake[0].x !== area.WIDTH && objSnake[0].y !== (area.HEIGHT) && objSnake[0].x !== area.WIDTH*(-1) && objSnake[0].y !== (area.HEIGHT*-1)){
-    elementSnake.style.marginLeft = (objSnake[0].x)*step + "px";
-    elementSnake.style.marginTop = (objSnake[0].y)*step + "px";
-  }
-  else {
-    alert("You lose");
-  }
-}
-
+};
 const myConstolSnake = function() {
   return controlSnake(axi, dir);
 };
-
 
 let timerId = setInterval(myConstolSnake, 1000);
 
 function moveMouse() {
   console.log('Оп Бля');
-    objMouse.x = getRandomInt(-8 , 8);
-    elementMouse.style.marginLeft = objMouse[0] + "px";
-    objMouse.y = getRandomInt(-6, 6);
-    elementMouse.style.marginTop = objMouse[1] + "px";
-}
+  objMouse.x = getRandomInt(-8 , 8);
+  objMouse.y = getRandomInt(-8, 8);
+  elementMouse.style.marginLeft = JSON.stringify(objMouse.x)*step + "px";
+  elementMouse.style.marginTop = JSON.stringify(objMouse.y)*step + "px";
+};
 
 function getRandomInt(min, max) {
   let rand = min - 0.5 + Math.random() * (max - min + 1);
   return Math.round(rand);
-}
+};
 document.addEventListener("keydown", key);
 
 // linear
